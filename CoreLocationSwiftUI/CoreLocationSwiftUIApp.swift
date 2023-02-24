0ef9917566d57c1afb8470602a9e688f0b7d6dc4
Malik A. Aziz Lubis <malik.lubis@gmail.com>
2023-02-25 00:55:07 +0700

    Initial Commit

---
+++ CoreLocationSwiftUI/CoreLocationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationSwiftUIApp.swift
+//  CoreLocationSwiftUI
+//
+//  Created by Malik A. Aziz Lubis on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
